654ef08c772e6d6a4a83262208536944e3301e91
Michael.altinisik1 <Michael.altinisik@iths.se>
2022-05-19 17:56:43 +0200

    Add redux functionality Add so you can fetch recipes from the api

---
+++ client/src/features/categories/categoriesSlice.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchCategories } from "../../API/categoris";
+
+const initialState = {
+  categories: [],
+};
+
+export const fetchCategoriesFromAPI = createAsyncThunk(
+  "categories/fetchCategories",
+  async () => {
+    const response = await fetchCategories();
+    return response.data;
+  }
+);
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategoriesFromAPI.fulfilled, (state, action) => {
+      state.categories = action.payload;
+    });
+  },
+});
+
+export default categoriesSlice.reducer;
